3af2e66af6f4db5eb7169ab5bd45764d04ad945c
Nikifor <nikiforn04@gmail.com>
2022-02-17 17:23:23 +0200

    start using styled components instead of sx styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,40 @@
 import { Typography, Button, Grid } from "@mui/material";
 import React from "react";
 import { IHeaderProps } from "../types";
+import styled from "@emotion/styled";
+import { addPrefix, StyleClassPrefix } from "./prefixes";
+
+const prefix = StyleClassPrefix.Header;
+
+export const headerClasses = {
+    appName: addPrefix(prefix, "appName"),
+    openFormButton: addPrefix(prefix, "openFormButton")
+};
+
+export const StyledHeader = styled(Grid)(({theme}) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: '15px',
+    backgroundColor: 'rgb(34, 34, 34)',
+    color: 'rgb(241, 143, 1)',
+    [`& .${headerClasses.appName}`]: {
+        fontSize: 32
+    },
+    [`& .${headerClasses.openFormButton}`]: {
+        margin: '10px',
+        backgroundColor: 'rgb(241, 143, 1)'
+    }
+}));
+
+const classes = headerClasses
 
 const Header = (props: IHeaderProps) => {
     return ( 
-        <Grid container
-        sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingLeft: '15px',
-            backgroundColor: 'rgb(34, 34, 34)',
-            color: 'rgb(241, 143, 1)'
-        }}>
-            <Typography variant='h1' sx={{fontSize: 32}}>Game Influencer</Typography>
-            <Button variant='contained' sx={{
-                margin: '10px',
-                color: 'rgb(34, 34, 34)',
-                backgroundColor: 'rgb(241, 143, 1)'
-            }} onClick={props.onClick}>{props.showForm ? 'Close' : 'Add Campaign' }</Button>
-        </Grid>
+        <StyledHeader container>
+            <Typography className={classes.appName} variant='h1'>Game Influencer</Typography>
+            <Button className={classes.openFormButton} variant='contained' onClick={props.onClick}>{props.showForm ? 'Close' : 'Add Campaign' }</Button>
+        </StyledHeader>
      );
 }
  
